d1330d799e3a400d85d227efc55654811b3fac28
tlacodnjs667 <tlacodnjs667@gmail.com>
2023-10-13 18:19:12 +0900

    피보나치 구현중

---
+++ src/com/euler/problem/EvenFibonacciNumbers.java
@@ -0,0 +1,31 @@
+package com.euler.problem;
+
+import java.util.TreeSet;
+
+public class EvenFibonacciNumbers {
+	public static int MAX = 4000000;
+	public static TreeSet<Integer> evens = new TreeSet<Integer>();
+
+	public static void main(String[] args) {
+
+		System.out.println(evens);
+	}
+
+	public static int fibonacci(int a) {
+
+		if (a == 1)
+			return 1;
+		if (a == 0)
+			return 1;
+
+		int result = fibonacci(a - 2) + fibonacci(a - 1);
+
+//		System.out.println("a = " + a + ", result =" + result);
+
+		if (result % 2 == 0)
+			evens.add(result);
+
+		return result;
+	}
+
+}
